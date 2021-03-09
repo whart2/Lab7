e673dfd2658e4fe64674b36891dbe487dfde13cf
treeden <37743981+treeden@users.noreply.github.com>
2021-03-08 21:44:59 -0500

    Initial commit

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,78 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * Adds support for running Rock, Paper, Scissors, Lizard, Spock
+ * @author Walden Hart
+ */
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * Checks if a choice made is valid or not
+     * @param pick choice for Rock, Paper, Scissors, Lizard, Spock
+     * @return true if the choice is valid, false otherwise
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * Randomly selects a choice in the Rock Paper Scissors Lizard Spock game
+     * @return a string representing a choice
+     */
+
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    // TODO remove this comment and document this method
+
+    /**
+     * Determines who wins in a game of Rock Paper Scissors Lizard Spock between a human and a computer
+     * @param c_pick the computer's choice
+     * @param h_pick the human's choice
+     * @return true if the computer wins, false if the human wins
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
